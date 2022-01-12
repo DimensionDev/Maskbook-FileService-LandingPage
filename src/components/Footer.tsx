d4e6fae1ec23ec81ac6f958469a6d8f6db6f6991
septs <github@septs.pw>
2022-01-12 11:53:19 +0800

    Update src/components/Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { useMetadata } from '../Metadata';
 
 const legal = 'https://legal.mask.io/arweave/file-service/';
 const repo = `https://github.com/DimensionDev/Maskbook-FileService-LandingPage/tree/${process.env.COMMIT_HASH}`;
-const defaultPrivacyLink = `https://legal.mask.io/maskbook/privacy-policy-browser.html`;
+const defaultPrivacyLink = 'https://legal.mask.io/maskbook/privacy-policy-browser.html';
 
 export default React.memo(() => {
   const meta = useMetadata();
