57dc47d260b451d8dbf70b2d3cf54f03ae5e2036
Septs <github@septs.pw>
2022-01-12 12:51:17 +0800

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,19 +4,24 @@ import { useMetadata } from '../Metadata';
 
 const legal = 'https://legal.mask.io/arweave/file-service/';
 const repo = `https://github.com/DimensionDev/Maskbook-FileService-LandingPage/tree/${process.env.COMMIT_HASH}`;
-const defaultPrivacyLink = 'https://legal.mask.io/maskbook/privacy-policy-browser.html';
+const defaultPrivacyLink =
+  'https://legal.mask.io/maskbook/privacy-policy-browser.html';
 
 export default React.memo(() => {
   const meta = useMetadata();
-  const privacyLink = !meta.provider ? legal : meta.provider === 'arweave' ? legal : defaultPrivacyLink;
   return (
     <nav>
-      <a href={privacyLink} target='_blank'>
+      <a href={getPrivacyLink(meta.provider)} target='_blank'>
         Legal Documents (Privacy, Copyright, etc)
       </a>
       <a className={locals.hidden} href={repo}>
         Generator version {process.env.COMMIT_DATE}
       </a>
     </nav>
-  ))
-};
+  );
+});
+
+function getPrivacyLink(provider: string) {
+  if (provider === 'arweave') return legal;
+  return defaultPrivacyLink;
+}
