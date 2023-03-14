fcbbbf8869cd328f5d3be5c5f4032cfbacd74101
unclebill <billbill290@gmail.com>
2023-03-14 14:49:10 +0800

    fix: recover files from ipfs

--- src/utils/recoverFile.ts
+++ src/utils/recoverFile.ts
@@ -1,19 +1,27 @@
 import { Metadata } from '../Metadata';
 
 const METADATA_OPEN_TAG = '<script type="text/json" id="metadata">';
-const PREVIEW_ORIGIN =
-  'https://2uhlk62ro3ctby7mehzurppjwc7tjuezhzthbjabmm2v6jtrnsba.arweave.net/';
 
-export async function recoverFile(url: string): Promise<Metadata> {
-  const html = await fetch(url).then((r) => r.text());
+export async function recoverFile(originUrl: string): Promise<Metadata> {
+  const html = await fetch(originUrl).then((r) => r.text());
   const start = html.indexOf(METADATA_OPEN_TAG) + METADATA_OPEN_TAG.length;
   const end = html.indexOf('</script>', start);
   const payload = html.slice(start, end);
   const metadata = JSON.parse(payload) as Metadata;
-  // Use page origin with confidence.
-  const origin = process.env.PREVIEW ? PREVIEW_ORIGIN : location.origin;
+  const url = new URL(originUrl);
+
+  // IPFS
+  if (url.hostname.includes('ipfs')) {
+    const metadataLink = new URL(metadata.link);
+    return {
+      ...metadata,
+      link: `${url.origin}${metadataLink.pathname}`,
+    };
+  }
+
+  // Arweave
   return {
     ...metadata,
-    link: origin + metadata.link.split('/').pop(),
+    link: `${url.origin}/${metadata.link.split('/').pop()}`,
   };
 }
